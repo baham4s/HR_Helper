0a4e4962391708745455b0d67438e8d22ea14736
simon <simon.p5207@gmail.com>
2022-01-11 14:17:59 +0100

    ajout composants

--- hrHelper/src/app/app.module.ts
+++ hrHelper/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FiltreComponent } from './composant/filtre/filtre.component';
+import { BarreDeRechercheComponent } from './composant/barre-de-recherche/barre-de-recherche.component';
+import { DisplayComponent } from './composant/display/display.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FiltreComponent,
+    BarreDeRechercheComponent,
+    DisplayComponent
   ],
   imports: [
     BrowserModule,
---
+++ hrHelper/src/app/composant/barre-de-recherche/barre-de-recherche.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barre-de-recherche',
+  templateUrl: './barre-de-recherche.component.html',
+  styleUrls: ['./barre-de-recherche.component.css']
+})
+export class BarreDeRechercheComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
