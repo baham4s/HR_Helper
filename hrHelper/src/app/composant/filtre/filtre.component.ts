bb9da708414372d568008f1ca843eb36a5abe9c4
simon <simon.p5207@gmail.com>
2022-01-16 16:23:28 +0100

    preparation des boutons des filtres avec les fonctions sans css

--- hrHelper/src/app/composant/filtre/filtre.component.ts
+++ hrHelper/src/app/composant/filtre/filtre.component.ts
@@ -7,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class FiltreComponent implements OnInit {
+  filtre:any=[];
   content: String ="";
+
+  ajouterFiltre(element:string){
+    let present=0;
+    console.log(present);
+
+    for(let i=0; i<this.filtre.length;i++){
+      if(this.filtre[i]== element){
+        present++;
+        console.log("ici");
+
+      }
+
+    }
+    if(present==0){
+      this.filtre+=element+"\n";
+
+    }
+  }
+
+  resetFiltre(){
+    this.filtre=[];
+  }
+
+
+
+
+
   ville(lieu: String){
     this.content=lieu;
   }
