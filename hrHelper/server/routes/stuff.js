efb67d45bcfa85bc8c45d852e278ff1c0b9e62e8
simon <simon.p5207@gmail.com>
2022-02-07 15:27:02 +0100

    test python

--- hrHelper/server/routes/stuff.js
+++ hrHelper/server/routes/stuff.js
@@ -28,8 +28,25 @@ router.get('', function(req, res) {
 // met a jour le filtre avec l 'id
 router.put('', (req, res, next) => {
   console.log(req.body)
+  // Use child_process.spawn method from
+  // child_process module and assign it
+  // to variable spawn
   var spawn = require("child_process").spawn;
-  var process = spawn('python',["./hello.py"] );
+
+  // Parameters passed in spawn -
+  // 1. type_of_script
+  // 2. list containing Path of the script
+  //    and arguments for the script
+
+  // E.g : http://localhost:3000/name?firstname=Mike&lastname=Will
+  // so, first name = Mike and last name = Will
+  var process = spawn('python',["../../latotal.py"] );
+
+  // Takes stdout data from script which executed
+  // with arguments and send this data to res object
+  process.stdout.on('data', function(data) {
+    res.send(data.toString());
+  } )
 
   filtre.updateOne({ _id: req.body._id }, { ...req.body, _id: req.body._id })
     .then(() => res.status(200).json({ message: 'Objet modifié !'}))
